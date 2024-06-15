06611da9f6b21e2496468275a80cf69c20d53ba9
Divyam <divgrover7@gmail.com>
2024-06-15 14:37:22 +0530

    fixed buggy vertical direction movement

--- script.js
+++ script.js
@@ -33,8 +33,8 @@ function updateSnake() {
   const head = { x: snake[0].x, y: snake[0].y };
   if (direction == "right") head.x += gridSize;
   if (direction == "left") head.x -= gridSize;
-  if (direction == "up") head.y += gridSize;
-  if (direction == "down") head.y -= gridSize;
+  if (direction == "up") head.y -= gridSize;
+  if (direction == "down") head.y += gridSize;
 
   if (head.x == food.x && head.y == food.y) {
     score++;
@@ -49,27 +49,32 @@ function updateSnake() {
 
   snake.unshift(head);
 
-
-if(head.x<0 || head.y <0 || head.x>=canvasSize || head.y>=canvasSize ) resetgame();
+  if (head.x < 0 || head.y < 0 || head.x >= canvasSize || head.y >= canvasSize)
+    resetgame();
 }
-function resetgame(){
-  direction='right';
-  snake=[{x:gridSize*5,y:gridSize*5}];
-  score=0;
-  document.getElementById('score').text='Score :'+score;
-  food={x:Math.floor(Math.random()*canvasSize/gridSize)*gridSize,y:Math.floor(Math.random()*canvasSize/gridSize)*gridSize}
+function resetgame() {
+  direction = "right";
+  snake = [{ x: gridSize * 5, y: gridSize * 5 }];
+  score = 0;
+  document.getElementById("score").text = "Score :" + score;
+  food = {
+    x: Math.floor((Math.random() * canvasSize) / gridSize) * gridSize,
+    y: Math.floor((Math.random() * canvasSize) / gridSize) * gridSize,
+  };
 }
 function changeDirection(event) {
   const keyPressed = event.key;
-  if (keyPressed === 'ArrowRight' && direction !== 'left') direction = 'right';
-  if (keyPressed === 'ArrowLeft' && direction !== 'right') direction = 'left';
-  if (keyPressed === 'ArrowUp' && direction !== 'down') direction = 'up';
-  if (keyPressed === 'ArrowDown' && direction !== 'up') direction = 'down';
+  if (keyPressed === "ArrowRight" && direction !== "left") direction = "right";
+  if (keyPressed === "ArrowLeft" && direction !== "right") direction = "left";
+  if (keyPressed === "ArrowUp" && direction !== "down") direction = "up";
+  if (keyPressed === "ArrowDown" && direction !== "up") direction = "down";
 }
-document.addEventListener('keydown',changeDirection);
-function gameloop(){
-  ctx.clearRect(0,0,canvasSize,canvasSize);
+document.addEventListener("keydown", changeDirection);
+function gameloop() {
+  ctx.clearRect(0, 0, canvasSize, canvasSize);
   drawSnake();
   drawFood();
   updateSnake();
 }
+
+setInterval(gameloop, 500);
